9eb4b490a59cf61a4280221faef4bececa4bdb52
Drew <amrtdc1@users.noreply.github.com>
2025-07-20 07:20:29 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,36 @@
-const CACHE_NAME = "photo-pwa-cache-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const CACHE_NAME = 'photo-pwa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './main.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((resp) => resp || fetch(event.request))
+    caches.match(event.request).then(cachedRes => {
+      return cachedRes || fetch(event.request).catch(() =>
+        new Response('You are offline and this resource is not cached.')
+      );
+    })
   );
 });
